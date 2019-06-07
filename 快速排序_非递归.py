a729416e78749dddefbb5efb1fce13a24b979b95
1050669722 <42562697+1050669722@users.noreply.github.com>
2019-06-07 14:29:29 +0800

    Update 快速排序_非递归.py

--- 快速排序_非递归.py
+++ 快速排序_非递归.py
@@ -5,41 +5,14 @@ Created on Tue Jun  4 19:47:29 2019
 @author: Administrator
 """
 
+'''
+Quick Sort | Non-Recursion | In-place
+'''
+
 class Solution:
     def sortArray(self, nums: list) -> list:
-#        return self.quick_sort(nums)
         self.quick_sort(nums)
         return nums
-    
-#    def quick_sort(self, nums):
-#        if len(nums) < 2:
-#            return nums
-#        stack = []
-#        stack.append(len(nums)-1)
-#        stack.append(0)
-#        while stack:
-#            l = stack.pop()
-#            r = stack.pop()
-#            idx = self.partition(nums, l, r)
-#            if l < idx-1:
-#                stack.append(idx-1)
-#                stack.append(l)
-#            if idx+1 < r:
-#                stack.append(r)
-#                stack.append(idx+1)
-#        return nums
-#        
-#    def partition(self, nums, left, right):
-#        key = nums[left]
-#        while left < right:
-#            while left < right and nums[right] >= key:
-#                right -= 1
-#            nums[left] = nums[right]
-#            while left < right and nums[left] <= key:
-#                left += 1
-#            nums[right] = nums[left]
-#        nums[left] = key
-#        return left
         
     def quick_sort(self, nums):
         if len(nums) <= 1:
@@ -69,22 +42,8 @@ class Solution:
             nums[right] = nums[left]
         nums[left] = key
         return left
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
         
 solu = Solution()
 nums = [3,7,6,4,1,9]
-#print(solu.quick_sort(nums))
 solu.sortArray(nums)
 print(nums)
